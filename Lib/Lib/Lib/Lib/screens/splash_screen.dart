5b76d8d7cc38f90ce9a9e4112bae991bef93eee7
Walefrncs <walefrncs@gmail.com>
2025-08-11 19:28:19 +0100

    Update splash_screen.dart

--- Lib/Lib/Lib/Lib/screens/splash_screen.dart
+++ Lib/Lib/Lib/Lib/screens/splash_screen.dart
@@ -1,1 +1,33 @@
+import 'package:flutter/material.dart';
+import '../routes.dart';
 
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Future.delayed(Duration(seconds: 2), () {
+      Navigator.pushReplacementNamed(context, Routes.home);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Icon(Icons.local_hospital, size: 72),
+            SizedBox(height: 12),
+            Text('Medical Support', style: TextStyle(fontSize: 24)),
+          ],
+        ),
+      ),
+    );
+  }
+}
